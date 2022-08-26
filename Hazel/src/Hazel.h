a65e0a1de1a6c2896d189bd4506aae7871873150
islander <miku.miku.miku@sjtu.edu.cn>
2022-08-26 20:37:58 +0800

    refactor(Hazel/*Code.h): HZ key codes

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -5,5 +5,10 @@
 #include "Hazel/Application.h"
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
+
+#include "Hazel/Input.h"
+#include "Hazel/KeyCode.h"
+#include "Hazel/MouseButtonCodes.h"
+
 #include "Hazel/ImGui/ImGuiLayer.h"
 
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -13,12 +13,19 @@ public:
 
 	void OnUpdate() override
 	{
-		HZ_INFO("ExampleLayer::Update");
+		//HZ_INFO("ExampleLayer::Update");
+
+		if (Hazel::Input::IsKeyPressed(HZ_KEY_TAB))
+			HZ_TRACE("Tab key is pressed!");
 	}
 
 	void OnEvent(Hazel::Event& event) override
 	{
-		HZ_TRACE("{0}", event.ToString());
+		if (event.GetEventType() == Hazel::EventType::KeyPressed)
+		{
+			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
+			HZ_TRACE("{0}", (char)e.GetKeyCode());
+		}
 	}
 };
 
