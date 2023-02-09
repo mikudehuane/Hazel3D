f310bff0c8d7a5a00ccc2ed9e7357d279d3bf888
islander <miku.miku.miku@sjtu.edu.cn>
2023-02-09 14:02:54 +0800

    build(scripts): build assimp in scripts

--- scripts/Win-GenerateProjects.bat
+++ scripts/Win-GenerateProjects.bat
@@ -1,4 +1,10 @@
 pushd ..\
 call vendor\bin\premake\premake5.exe vs2022
 popd
+pushd ..\Hazel\vendor\assimp\
+cmake CMakeLists.txt 
+cmake --build .
+popd
+copy ..\Hazel\vendor\assimp\bin\Debug\assimp-vc143-mtd.dll ..\bin\Debug-windows-x86_64\Sandbox\
+copy ..\Hazel\vendor\assimp\bin\Debug\assimp-vc143-mtd.dll ..\bin\Release-windows-x86_64\Sandbox\
 PAUSE
