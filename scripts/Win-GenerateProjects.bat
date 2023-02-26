669fed8d621c6f84d7ba934f04a9d499b9c0173b
islander <miku.miku.miku@sjtu.edu.cn>
2023-02-26 12:06:02 +0800

    build(Win-GenerateProjects.bat): mkdir

--- scripts/Win-GenerateProjects.bat
+++ scripts/Win-GenerateProjects.bat
@@ -5,6 +5,8 @@ pushd ..\Hazel\vendor\assimp\
 cmake CMakeLists.txt 
 cmake --build . --config Release
 popd
+mkdir ..\bin\Debug-windows-x86_64\Sandbox
+mkdir ..\bin\Release-windows-x86_64\Sandbox
 copy ..\Hazel\vendor\assimp\bin\Release\assimp-vc143-mt.dll ..\bin\Debug-windows-x86_64\Sandbox\
 copy ..\Hazel\vendor\assimp\bin\Release\assimp-vc143-mt.dll ..\bin\Release-windows-x86_64\Sandbox\
 echo "TODO: download the model files to Sandbox\assets\"
